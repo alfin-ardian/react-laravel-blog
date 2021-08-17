a56d789a2753e1308734e64d5f8c4b48ab552891
alfin ardian <developer.alfin@gmail.com>
2021-08-17 11:00:00 +0700

    part 1- update register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -68,5 +69,29 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        // Here the request is validated. The validator method is located
+        // inside the RegisterController, and makes sure the name, email
+        // password and password_confirmation fields are required.
+        $this->validator($request->all())->validate();
+        $user = $this->create($request->all());
+        // After the user is created, he's logged in.
+        $this->guard()->login($user);
+        // And finally this is the hook that we want. If there is no
+        // registered() method or it returns null, redirect him to
+        // some other URL. In our case, we just need to implement
+        // that method to return the correct response.
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
+    }
+
+    protected function registered(Request $request, $user)
+    {
+        $user->api_token = Str::random(60);
+        $user->save();
+        return response()->json(['user' => $user->toArray()], 201);
     }
 }
