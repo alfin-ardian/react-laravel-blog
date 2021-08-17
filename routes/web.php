b51d9cfaf620ef7f9683f5982316b0b3c8a5e35c
alfin ardian <developer.alfin@gmail.com>
2021-08-17 11:54:17 +0700

    part 3 - route web

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Website route
+Route::get('/{path?}', function () {
+    return view('website');
+})->where('path', '[^admin]*');
+// Admin route
+Route::get('/admin/{path?}', function () {
+    return view('admin');
+})->where('path', '.*');
